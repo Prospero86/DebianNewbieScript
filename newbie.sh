485f7c8bd5286207398c01c47321a27976f4d625
Chocolate D. Chip <chocolatechip@derpymail.org>
2015-08-31 21:49:45 -0700

    Minor fix for 0.0.9 Final Alpha
    Wow. I messed up again. Should be fixed now though!

--- newbie.sh
+++ newbie.sh
@@ -220,7 +220,7 @@ wget https://addons.mozilla.org/firefox/downloads/latest/607454/addon-607454-lat
 wget https://mega.nz/meganz.xpi -O iceweasel-plugins/mega.xpi
 mkdir ~/Music/.startup
 wget http://www.windows93.net/c/sys/boot/boot.ogg -O ../Music/.startup/startup.ogg
-echo "@play -t ogg -v 0.40 ~/Music/startup.ogg
+echo "@play -t ogg -v 0.40 ~/Music/.startup/startup.ogg
 @redshift-gtk
 @qtox
 @lxpanel --profile LXDE
